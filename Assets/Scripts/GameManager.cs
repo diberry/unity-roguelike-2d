6dca3037884425bf169069fc8cddcfc0ddd2aed7
Dina Berry <diberry@microsoft.com>
2019-03-17 09:26:38 -0700

    game manager singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    // singleton - only 1 allowed
+    public static GameManager instance = null;
+
     public BoardManager boardScript;
 
     // for testing
@@ -15,6 +18,15 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        // make sure only have 1 game manager
+        if (instance == null)
+            instance = this;
+        else if(instance != this)
+            Destroy(gameObject);
+
+        // don't destry between scense
+        DontDestroyOnLoad(gameObject);
+
         boardScript = GetComponent<BoardManager>();
         InitGame();
     }
