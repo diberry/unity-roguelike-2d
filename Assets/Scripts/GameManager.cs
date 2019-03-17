755e1e96f0a159941621d5c2f7a0346e23df734d
Dina Berry <diberry@microsoft.com>
2019-03-17 06:34:22 -0700

    every setup to test level 3 but camera is out of position

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public BoardManager boardScript;
+
+    // for testing
+    // 3 = where enemies appear
+    private int level = 3;
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
